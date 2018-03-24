d62819d1bc4de4649ad364d50361661f8c4895de
siddhartha <siddharthak024@gmail.com>
2018-03-24 19:54:30 -0400

    refactor

---
+++ src/test/java/com/target/myretail/MyRetailTest.java
@@ -0,0 +1,13 @@
+package com.target.myretail;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyRetailTest {
+
+	@Test
+	public void AppTest() {
+		assertEquals(1, 1);
+	}
+}
